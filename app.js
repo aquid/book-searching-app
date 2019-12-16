2cbbddbb9a2ba07b116343ba205d6e082711fc5c
Aquid Shahwar <aquid.shahwar@gmail.com>
2019-12-16 11:32:59 +0530

    Finalised bugs and created build for testing

--- app.js
+++ app.js
@@ -20,9 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', indexRouter);
 app.use('/api/books', booksRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+});
 
 module.exports = app;
